7eceddbc7a30ebd1106df2ddf721fe08b3242f33
Magyar Tamás <tamas.donix@gmail.com>
2021-01-06 14:55:42 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,11 @@
 module.exports = {
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.PORT || 80,
     AUTH: {
         user: 'jofogas.watcher@gmail.com',
-        pass: 'Sv5g9pQjtJ4xr5p8M54Z5Qq2tYvDbSTetYRQpYVZz4rJjGmZ34NX6qWEqMBLDpLG'
+        pass: 'Sv5g9pQjtJ4xr5p8M54Z5Qq2tYvDbSTatYRQpYVZz4rJjGmZ34NX6qWEqMBLDpLG'
     },
-    USER: "tamas.donix@gmail.com",
+    USER: "donix@gmail.com",
     SCRAP: 5,
-    NOTIFY: 8
+    NOTIFY: 8,
+    MASTER: 'jofogas'
 }
