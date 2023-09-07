db6731e2ce4b2fd21c46047efb2a045ba42964b8
imdonix <tamas.donix@gmail.com>
2023-09-07 23:38:38 +0200

    added execution cooldown

--- services/executor.js
+++ services/executor.js
@@ -1,16 +1,19 @@
+const moment = require('moment')
+
 const { Item } = require('./db')
 
 const Jofogas = require('../srappers/jofogas');
 const Ingatlan = require('../srappers/ingatlan');
 const Auto = require('../srappers/auto');
 
-const { niceDate, dateOnly } = require('./time')
+const { niceDate, dateOnly } = require('./time');
+const { range } = require('express/lib/request');
 
 class Executor
 {
     scrappers;
     queue;
-
+    cooldown;
 
     constructor()
     {
@@ -19,6 +22,9 @@ class Executor
         const auto = new Auto()
 
         this.scrappers = [jofogas]
+        this.cooldown = new Map()
+        this.cooldown[jofogas] = moment()
+
         this.queue = Array()
 
         this.start()
@@ -56,24 +62,39 @@ class Executor
             const next = this.queue.shift()
 
             const engine = this.scrappers.find(scrapper => scrapper.id() == next.routine.engine)
-            const result = await engine.scrapPage(next.routine, next.page)
-
-            for(let found of result.items)
+            if(engine)
             {
-                found.found = niceDate()
-                await Item.create({
-                    id: found.id,
-                    sent: false,
-                    owner: next.owner,
-                    json: JSON.stringify(found)
-                }, {
-                    ignoreDuplicates: true
-                })
+                if (this.cooldown[engine] < moment())
+                {
+                    const result = await engine.scrapPage(next.routine, next.page)
+                    this.cooldown[engine] = moment().add(Math.max(5, Math.floor(Math.random() * 15)), 's')
+
+                    for(let found of result.items)
+                    {
+                        found.found = niceDate()
+                        await Item.create({
+                            id: found.id,
+                            sent: false,
+                            owner: next.owner,
+                            json: JSON.stringify(found)
+                        }, {
+                            ignoreDuplicates: true
+                        })
+                    }
+                    
+                    this.enqueue(next.owner, next.routine, result.page)
+                    console.log(`[${niceDate()}] |${next.owner}| <${next.page}> /${engine.id()}->${next.routine.keywords}/ -> found ${result.items.length} `)    
+                }
+                else
+                {
+                    this.queue.push(next)
+                }
+           }
+            else
+            {
+                console.log(`[${niceDate()}] [Executor] Cannot find engine with name '${next.routine.engine}'`)
             }
-            
-            this.enqueue(next.owner, next.routine, result.page)
-            console.log(`[${niceDate()}] |${next.owner}| <${next.page}> /${engine.id()}->${next.routine.keywords}/ -> found ${result.items.length} `)
-        }
+       }
     }
 }
 
